0cf2751a303bc4e4e47d77440b85fa4231e7a0a4
John Nelson <john@mikelopezuniversity.ml>
2021-01-23 00:11:23 -0600

    add multipication logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-alert(subtract(number1, number2));
+alert(multiply(number1, number2));
